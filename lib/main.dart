9c5e89c9063c13de4913fc483d6ce7286f5dfbab
cherotich <carolcaro60@gmail.com>
2020-05-18 23:49:48 +0300

    Details page

--- lib/main.dart
+++ lib/main.dart
@@ -1,5 +1,7 @@
 import 'package:flutter/material.dart';
 
+import 'detailsPage.dart';
+
 void main() {
   runApp(MyApp());
 }
@@ -106,6 +108,70 @@ class _MyHomePageState extends State<MyHomePage> {
                       ],
                     ),
                   ),
+                ),
+                Row(
+                  mainAxisAlignment: MainAxisAlignment.spaceEvenly,
+           children: <Widget>[
+             Container(
+               height: 65.0,
+               width: 60.0,
+               decoration: BoxDecoration(
+                 border: Border.all(
+                   color: Colors.grey,
+                   style:BorderStyle.solid
+                   ,width: 1.0
+                 ),
+                 borderRadius: BorderRadius.circular(10.0)
+               ),
+               child: Center(
+                 child: Icon(Icons.search,
+                 color: Colors.black,
+                  ),
+               ),
+             ),
+
+     Container(
+               height: 65.0,
+               width: 60.0,
+               decoration: BoxDecoration(
+                 border: Border.all(
+                   color: Colors.grey,
+                   style:BorderStyle.solid,
+                   width: 1.0
+                 ),
+                 borderRadius: BorderRadius.circular(10.0)
+               ),
+               child: Center(
+                 child: Icon(Icons.shopping_basket,
+                 color: Colors.black,
+                  ),
+               ),
+             ),
+
+     Container(
+               height: 65.0,
+               width: 120.0,
+               decoration: BoxDecoration(
+                 border: Border.all(
+                   color: Colors.black,
+                   style:BorderStyle.solid,
+                   width: 1.0
+                 ),
+                 borderRadius: BorderRadius.circular(10.0),
+                  color: Color(0xFF1C1428)
+               ),
+               child: Center(
+                 child: Text('Checkout',
+                 style:TextStyle(
+                   fontFamily: 'Montserrat',
+                   color:Colors.white,
+                   fontSize: 15.0
+                 ))
+               ),
+             ),
+
+
+           ],
                 )
               ],
             ),
@@ -119,7 +185,15 @@ class _MyHomePageState extends State<MyHomePage> {
     return Padding(
       padding: EdgeInsets.only(left: 10.0, right: 10.0, top: 10.0),
       child: InkWell(
-        onTap: () {},
+        onTap: () {
+          Navigator.of(context)
+          .push(MaterialPageRoute(
+            builder: (context)=>
+            DetailsPage(heroTag:imgPath,foodName:foodName,foodPrice:price)
+            
+            
+            ));
+        },
         child: Row(
           mainAxisAlignment: MainAxisAlignment.spaceBetween,
           children: <Widget>[
